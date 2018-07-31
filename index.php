737845187e9291445207fe3b720a593d41718340
dembasy <dembasy1227@gmail.com>
2018-07-31 15:43:59 +0200

    done

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$region = array(
+  '59' => 'Nord',
+  '62' => 'Pas de Calais',
+  '02'=> 'Aisne',
+  '60' => 'Oise',
+  '80' => 'Somme');
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title> exercice 1</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        echo $region['59'];
+        ?>
+    </p>
+  </body>
+</html>
